a6ebb9fba192529550353c1fc3a14078019de539
Quentin MALINGE <quentin.malinge@gmail.com>
2019-03-11 11:20:25 +0100

    Ajout de test unitaires

--- ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailComparatorTest.java
+++ ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailComparatorTest.java
@@ -22,19 +22,31 @@ public class MailComparatorTest {
 	public final void egauxSiDeuxMailsNuls() {
 		Mail mail1 = null;
 		Mail mail2 = null;
-		assertThat(comparator.compare(mail1, mail2), is(0));
+		assertThat(comparator.compare(mail1, mail2), is(comparator.EGAUX));
 	}
 	
 	@Test
 	public final void egauxSiUnDesMailsNuls() {
 		Mail mail1 = new Mail();
 		Mail mail2 = null;
-		assertThat(comparator.compare(mail1, mail2), is(0));
+		assertThat(comparator.compare(mail1, mail2), is(comparator.EGAUX));
 	}
 	
-	//TODO
 	//Autres tests unitaires
 	
+	@Test
+	public final void statutDifferentsMails() {
+		Mail mail1 = new Mail.Builder("Mail test").statut(Mail.Statut.UNSENT).build();
+		Mail mail2 = new Mail.Builder("Mail test").statut(Mail.Statut.SENT).build();
+		assertThat(comparator.compare(mail1, mail2), is(comparator.PREMIER_PLUS_PETIT));
+	}
+	
+	@Test
+	public final void sujetDifferentsMails() {
+		Mail mail1 = new Mail.Builder("Sujet mail 1").statut(Mail.Statut.SENT).build();
+		Mail mail2 = new Mail.Builder("Sujet mail 2").statut(Mail.Statut.SENT).build();
+		assertThat(comparator.compare(mail1, mail2), is(comparator.PREMIER_PLUS_GRAND));
+	}
 	
 	
 }
